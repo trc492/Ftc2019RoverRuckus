d9daec2ef611940261b4a84907a47875e92dfc83
trc492 <trc492@live.com>
2018-10-30 17:05:39 -0700

    Sync'd with SDK 4.2.

---
+++ TeamCode/src/main/java/common/FtcTestPixyCam.java
@@ -0,0 +1,77 @@
+package common;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import ftclib.FtcOpMode;
+import ftclib.FtcPixyCam;
+import hallib.HalDashboard;
+import trclib.TrcDbgTrace;
+import trclib.TrcPixyCam;
+import trclib.TrcRobot;
+
+@TeleOp(name="Test: FTC Pixy Test", group="Test")
+@Disabled
+public class FtcTestPixyCam extends FtcOpMode
+{
+    private static final boolean USE_TRACELOG = true;
+
+    private HalDashboard dashboard;
+    private FtcPixyCam pixyCam;
+    private TrcDbgTrace tracer;
+
+    @Override
+    public void initRobot()
+    {
+        dashboard = HalDashboard.getInstance();
+        pixyCam = new FtcPixyCam("pixy");
+        tracer = FtcOpMode.getGlobalTracer();
+
+        if (USE_TRACELOG)
+        {
+            tracer.openTraceLog("/sdcard/FIRST/tracelog", "PixyCam");
+            tracer.traceInfo("Init", "Starting logging...");
+        }
+    }   //initRobot
+
+    @Override
+    public void startMode(TrcRobot.RunMode runMode)
+    {
+        if (USE_TRACELOG)
+        {
+            tracer.setTraceLogEnabled(true);
+        }
+        pixyCam.setEnabled(true);
+    }
+
+    @Override
+    public void stopMode(TrcRobot.RunMode runMode)
+    {
+        pixyCam.setEnabled(false);
+
+        if (USE_TRACELOG)
+        {
+            tracer.setTraceLogEnabled(false);
+            tracer.closeTraceLog();
+        }
+    }
+
+    @Override
+    public void runPeriodic(double elapsedTime)
+    {
+        TrcPixyCam.ObjectBlock[] objects = pixyCam.getDetectedObjects();
+
+        if (objects != null)
+        {
+            dashboard.displayPrintf(1, "Detected objects=%d.", objects.length);
+            for (int i = 0; i < objects.length; i++)
+            {
+                dashboard.displayPrintf(2 + i, "%d: %d,%d(%d,%d)/%d",
+                        objects[i].signature,
+                        objects[i].centerX, objects[i].centerY, objects[i].width,
+                        objects[i].height, objects[i].angle);
+            }
+        }
+    }   //runPeriodic
+
+}   //FtcTestPixyCam
---
+++ TeamCode/src/main/java/common/FtcTestPixyCamRaw.java
@@ -0,0 +1,54 @@
+package common;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+
+import ftclib.FtcI2cDeviceSynch;
+import ftclib.FtcOpMode;
+import hallib.HalDashboard;
+import trclib.TrcRobot;
+
+@TeleOp(name="Test: FTC Raw Pixy Test", group="Test")
+@Disabled
+public class FtcTestPixyCamRaw extends FtcOpMode
+{
+    private HalDashboard dashboard;
+    private FtcI2cDeviceSynch pixyCam;
+
+    @Override
+    public void initRobot()
+    {
+        dashboard = HalDashboard.getInstance();
+        pixyCam = hardwareMap.get(FtcI2cDeviceSynch.class, "pixy");
+        pixyCam.setDeviceInfo(HardwareDevice.Manufacturer.Other, "Pixy Camera v1");
+        pixyCam.setI2cAddress(0x54, true);
+    }   //initRobot
+
+    @Override
+    public void startMode(TrcRobot.RunMode runMode)
+    {
+        pixyCam.getDeviceClient().engage();
+    }   //startMode
+
+    @Override
+    public void stopMode(TrcRobot.RunMode runMode)
+    {
+        pixyCam.getDeviceClient().disengage();
+    }   //stopMode
+
+    @Override
+    public void runPeriodic(double elapsedTime)
+    {
+        byte[] data = pixyCam.readData(0,14);
+        if (data != null)
+        {
+            for (int i = 0; i + 1 < data.length; i += 2)
+            {
+                if (dashboard != null)
+                dashboard.displayPrintf(i/2 + 1, "%02d: %02x, %02x", i, data[i], data[i + 1]);
+            }
+        }
+    }   //runPeriodic
+
+}   //FtcTestPixyCamRaw
