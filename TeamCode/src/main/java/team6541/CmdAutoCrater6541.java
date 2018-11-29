40960e7b8d7a30b1d1bbddaea06f78b3b3fd979f
trc492 <trc492@live.com>
2018-11-29 12:37:51 -0800

    Tuned 3543 Crater autonomous. Enabled odometry task in 6541. Removed obsolete autonomous.

--- TeamCode/src/main/java/team6541/CmdAutoCrater6541.java
+++ TeamCode/src/main/java/team6541/CmdAutoCrater6541.java
@@ -178,7 +178,7 @@ public class CmdAutoCrater6541 implements TrcRobot.RobotCommand
                     robot.elevator.setPosition(RobotInfo6541.ELEVATOR_MIN_HEIGHT);
                     targetX = 0.0;
                     targetY = 0.0;
-                    robot.targetHeading = -90.0;
+                    robot.targetHeading = -80.0;
                     robot.pidDrive.setTarget(targetX, targetY, robot.targetHeading, false, event);
                     sm.waitForSingleEvent(event, State.DRIVE_TO_WALL);
                     break;
@@ -199,7 +199,7 @@ public class CmdAutoCrater6541 implements TrcRobot.RobotCommand
                     //
                     targetX = 0.0;
                     targetY = 0.0;
-                    robot.targetHeading -= 45.0;
+                    robot.targetHeading -= 55.0;
                     nextState = doTeamMarker? State.DRIVE_TO_DEPOT: State.DRIVE_FROM_MID_WALL_TO_CRATER;
                     robot.pidDrive.setTarget(targetX, targetY, robot.targetHeading, false, event);
                     sm.waitForSingleEvent(event, nextState);
@@ -210,7 +210,7 @@ public class CmdAutoCrater6541 implements TrcRobot.RobotCommand
                     // Go to the depot to drop off team marker.
                     //
                     targetX = 0.0;
-                    targetY = 36.0;
+                    targetY = 48.0;
                     robot.pidDrive.setTarget(targetX, targetY, robot.targetHeading, false, event);
                     sm.waitForSingleEvent(event, State.DROP_TEAM_MARKER);
                     break;
@@ -229,7 +229,7 @@ public class CmdAutoCrater6541 implements TrcRobot.RobotCommand
                     // Go to the crater and park there.
                     //
                     targetX = 0.0;
-                    targetY = -72.0;
+                    targetY = -85.0;
                     robot.pidDrive.setTarget(targetX, targetY, robot.targetHeading, false, event);
                     sm.waitForSingleEvent(event, State.DONE);
                     break;
